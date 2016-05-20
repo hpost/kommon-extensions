bc6d4b1eb695deaacdc85e3f84ad88de2ba3ac64
Henning Post <henningpost@gmail.com>
2016-05-20 13:38:29 -0700

    Debug log level

--- kommon-extensions/src/main/kotlin/cc/femto/kommon/extensions/Log.kt
+++ kommon-extensions/src/main/kotlin/cc/femto/kommon/extensions/Log.kt
@@ -4,6 +4,8 @@ import timber.log.Timber
 
 inline fun Any.v(msg: String, t: Throwable? = null) = Timber.v(t, msg)
 
+inline fun Any.d(msg: String, t: Throwable? = null) = Timber.d(t, msg)
+
 inline fun Any.i(msg: String, t: Throwable? = null) = Timber.i(t, msg)
 
 inline fun Any.w(msg: String, t: Throwable? = null) = Timber.w(t, msg)
