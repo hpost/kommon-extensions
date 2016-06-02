030d292bd41507a6443a9a6322af4385b13068f0
Henning Post <henningpost@gmail.com>
2016-06-02 13:25:51 -0700

    removing clamp functions in favor of coerceIn from stdlib

--- kommon-extensions/src/main/kotlin/cc/femto/kommon/extensions/Math.kt
+++ kommon-extensions/src/main/kotlin/cc/femto/kommon/extensions/Math.kt
@@ -1,9 +1,3 @@
 package cc.femto.kommon.extensions
 
 inline fun ld(exponent: Int) = Math.pow(2.0, exponent.toDouble()).toInt()
-
-inline fun Int.clamp(min: Int, max: Int) = Math.min(Math.max(this, min), max)
-inline fun Long.clamp(min: Long, max: Long) = Math.min(Math.max(this, min), max)
-inline fun Float.clamp(min: Float, max: Float) = Math.min(Math.max(this, min), max)
-inline fun Double.clamp(min: Double, max: Double) = Math.min(Math.max(this, min), max)
-
