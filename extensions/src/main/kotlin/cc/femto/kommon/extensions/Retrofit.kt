01d12fbca5cba0d6f460e2e5a0bba43983d01182
Henning Post <henningpost@gmail.com>
2016-09-22 17:27:42 -0700

    Extension function on Observable<Result<T>> for multiplexing into success and error Observables

--- extensions/src/main/kotlin/cc/femto/kommon/extensions/Retrofit.kt
+++ extensions/src/main/kotlin/cc/femto/kommon/extensions/Retrofit.kt
@@ -1,4 +1,19 @@
 package cc.femto.kommon.extensions
 
-val <T> retrofit2.adapter.rxjava.Result<T>.isSuccess: Boolean
+import retrofit2.adapter.rxjava.Result
+import rx.Observable
+
+val <T> Result<T>.isSuccess: Boolean
     get() = !isError && response().isSuccessful
+
+/**
+ * Multiplex the result Observable into success and error Observables.
+ * Note that both the returned success and error Observables
+ * will need a subscriptions to start the stream.
+ *
+ * @return An Observable pair which filters the source for success and error results
+ */
+fun <S, T : Result<S>> Observable<T>.multiplexResult(): Pair<Observable<T>, Observable<T>> {
+    val result = this.publish().autoConnect(2)
+    return result.filter { it.isSuccess } to result.filter { !it.isSuccess }
+}
