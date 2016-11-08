992f788ad9d2aa7010336ac745d3508c70aebd29
Henning Post <henningpost@gmail.com>
2016-11-08 10:17:06 -0800

    Set compound drawables with intrinsic bounds

--- extensions/src/main/kotlin/cc/femto/kommon/extensions/Button.kt
+++ extensions/src/main/kotlin/cc/femto/kommon/extensions/Button.kt
@@ -5,28 +5,28 @@ import android.widget.Button
 
 var Button.compoundDrawableStart: Drawable?
     get() = compoundDrawablesRelative[0]
-    set(value) = setCompoundDrawablesRelative(value,
+    set(value) = setCompoundDrawablesRelativeWithIntrinsicBounds(value,
             compoundDrawablesRelative[1],
             compoundDrawablesRelative[2],
             compoundDrawablesRelative[3])
 
 var Button.compoundDrawableTop: Drawable?
     get() = compoundDrawablesRelative[1]
-    set(value) = setCompoundDrawablesRelative(compoundDrawablesRelative[0],
+    set(value) = setCompoundDrawablesRelativeWithIntrinsicBounds(compoundDrawablesRelative[0],
             value,
             compoundDrawablesRelative[2],
             compoundDrawablesRelative[3])
 
 var Button.compoundDrawableEnd: Drawable?
     get() = compoundDrawablesRelative[2]
-    set(value) = setCompoundDrawablesRelative(compoundDrawablesRelative[0],
+    set(value) = setCompoundDrawablesRelativeWithIntrinsicBounds(compoundDrawablesRelative[0],
             compoundDrawablesRelative[1],
             value,
             compoundDrawablesRelative[3])
 
 var Button.compoundDrawableBottom: Drawable?
     get() = compoundDrawablesRelative[3]
-    set(value) = setCompoundDrawablesRelative(compoundDrawablesRelative[0],
+    set(value) = setCompoundDrawablesRelativeWithIntrinsicBounds(compoundDrawablesRelative[0],
             compoundDrawablesRelative[1],
             compoundDrawablesRelative[2],
             value)
--- extensions/src/main/kotlin/cc/femto/kommon/extensions/TextView.kt
+++ extensions/src/main/kotlin/cc/femto/kommon/extensions/TextView.kt
@@ -9,28 +9,28 @@ var TextView.textColor: Int
 
 var TextView.compoundDrawableStart: Drawable?
     get() = compoundDrawablesRelative[0]
-    set(value) = setCompoundDrawablesRelative(value,
+    set(value) = setCompoundDrawablesRelativeWithIntrinsicBounds(value,
             compoundDrawablesRelative[1],
             compoundDrawablesRelative[2],
             compoundDrawablesRelative[3])
 
 var TextView.compoundDrawableTop: Drawable?
     get() = compoundDrawablesRelative[1]
-    set(value) = setCompoundDrawablesRelative(compoundDrawablesRelative[0],
+    set(value) = setCompoundDrawablesRelativeWithIntrinsicBounds(compoundDrawablesRelative[0],
             value,
             compoundDrawablesRelative[2],
             compoundDrawablesRelative[3])
 
 var TextView.compoundDrawableEnd: Drawable?
     get() = compoundDrawablesRelative[2]
-    set(value) = setCompoundDrawablesRelative(compoundDrawablesRelative[0],
+    set(value) = setCompoundDrawablesRelativeWithIntrinsicBounds(compoundDrawablesRelative[0],
             compoundDrawablesRelative[1],
             value,
             compoundDrawablesRelative[3])
 
 var TextView.compoundDrawableBottom: Drawable?
     get() = compoundDrawablesRelative[3]
-    set(value) = setCompoundDrawablesRelative(compoundDrawablesRelative[0],
+    set(value) = setCompoundDrawablesRelativeWithIntrinsicBounds(compoundDrawablesRelative[0],
             compoundDrawablesRelative[1],
             compoundDrawablesRelative[2],
             value)
